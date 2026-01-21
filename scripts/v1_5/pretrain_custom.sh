41ccd67843bc6139962516dc90905456f5b7b433
paul.liu <paul.liu@cxmt.com>
2026-01-21 13:29:52 +0800

    严格遵循LLaVA 1.5的初始设定

--- scripts/v1_5/pretrain_custom.sh
+++ scripts/v1_5/pretrain_custom.sh
@@ -25,7 +25,7 @@ deepspeed llava/train/train_mem.py \
     --save_strategy "steps" \
     --save_steps 24000 \
     --save_total_limit 1 \
-    --learning_rate 2e-5 \
+    --learning_rate 1e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
