801b6960675497bf60d7cc1251538192c1796ba0
paul.liu <paul.liu@cxmt.com>
2026-02-06 13:04:23 +0800

    解决finetune OOM的问题

--- scripts/v1_5/finetune_qwen3.sh
+++ scripts/v1_5/finetune_qwen3.sh
@@ -22,9 +22,9 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir "${FINETUNE_DIR}" \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
