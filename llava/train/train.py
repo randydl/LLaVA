f053239af3b368d99e45c828119c9570995dd21e
paul.liu <paul.liu@cxmt.com>
2026-02-09 16:45:39 +0800

    [rank1]: ValueError: Loading this model requires you to execute custom code contained in the model repository on your local machine. Please set the option  to permit loading of this model.

--- llava/train/train.py
+++ llava/train/train.py
@@ -913,6 +913,7 @@ def train(attn_implementation=None):
                 cache_dir=training_args.cache_dir,
                 attn_implementation=attn_implementation,
                 torch_dtype=(torch.bfloat16 if training_args.bf16 else None),
+                trust_remote_code=True,
                 **bnb_model_from_pretrained_args
             )
         else:
